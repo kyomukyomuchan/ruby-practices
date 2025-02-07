e13d977f6c8d9302da61de4e0909d70d9f67a06a
kyomu-ai <aika.80222@gmail.com>
2025-02-07 17:50:26 +0900

    サイズ１の要素に対してjoinを取り除き、ループしてから中央に配置した

--- cal.rb
+++ cal.rb
@@ -16,13 +16,13 @@ last_date = Date.new(year, month, -1)
  
 if (1700..2100).cover?(year)
   month_year = [month.to_s + "月" + year.to_s]
-  month_year_s = month_year.join("")
-  result = month_year_s.center(20)
-  puts result
-
+  
+  month_year.each do |month_and_year|
+    puts month_and_year.center(20)
+  end
   wdays = ["日", "月", "火", "水", "木", "金", "土"] 
   puts wdays.join(" ")
-
+  
   carender = (begining_date..last_date).map do |hiniti|
     if hiniti == begining_date
       spece = begining_date.wday * 3 
