79ad73be39c586b72b2f85c7710d6972c4f352b7
kyomu-ai <aika.80222@gmail.com>
2025-02-07 17:20:42 +0900

    範囲オブジェクトで読みやすくした

--- cal.rb
+++ cal.rb
@@ -14,7 +14,7 @@ year = opts["y"].to_i
 begining_date = Date.new(year, month, 1)
 last_date = Date.new(year, month, -1)
  
-if year >= 1970 && year <= 2100
+if (1700..2100).cover?(year)
   month_year = [month.to_s + "月" + year.to_s]
   month_year_s = month_year.join("")
   result = month_year_s.center(20)
