0ba230e73d04dcb9e9b92c74b870d6e45395e1e9
kyomu-ai <aika.80222@gmail.com>
2025-02-17 16:19:13 +0900

    年月を式展開へ変更

--- cal.rb
+++ cal.rb
@@ -15,7 +15,7 @@ begining_date = Date.new(year, month, 1)
 last_date = Date.new(year, month, -1)
  
 if (1700..2100).cover?(year)
-  puts "      " + month.to_s + "月" + year.to_s
+  puts "      #{month}月#{year}"
   wdays = ["日", "月", "火", "水", "木", "金", "土"] 
   puts wdays.join(" ")
 
